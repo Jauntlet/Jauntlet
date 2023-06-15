566a735e70b5e69f85b6ccb1bff7800243588cf6
Xander Mooney <ps24XMooney@efcts.us>
2023-06-14 22:38:45 -0400

    Caching changes

--- Jauntlet/TextureCache.h
+++ Jauntlet/TextureCache.h
@@ -7,15 +7,15 @@
 // TextureCache is a smarter way of storing textures so that reused textures aren't loaded multiple times. It uses maps to allow quick finding and usage of the texture.
 
 namespace Jauntlet {
-	class TextureCache
-	{
-	public:
-		TextureCache();
+class TextureCache
+{
+public:
+	TextureCache();
 
-		// gets a texture based on filepaths
-		GLTexture getTexture(std::string filePath);
-	private:
-		std::map<std::string, GLTexture> _textureMap;
-	};
+	// gets a texture based on filepaths
+	GLTexture getTexture(std::string filePath);
+private:
+	std::map<std::string, GLTexture> _textureMap;
+};
 }
 
