ab41814d7b84718fcf20000da8092cdabe2ceef5
PlayStation24Chris <PS24CKowalewski@efcts.us>
2023-08-20 15:28:07 -0400

    When Worlds Jollide
    EXPLAINED: when i use the names "collision" or "collider" etc., its already in use so it breaks.

--- Jauntlet/Collision2D.cpp
+++ Jauntlet/Collision2D.cpp
@@ -4,17 +4,17 @@
 
 using namespace Jauntlet;
 
-Collision2D::Collision2D(Collider2D parent, Collider2D other) //constructor
+JCollision2D::JCollision2D(JCollider2D parent, JCollider2D other) : _parent(parent), _other(other) //constructor
 {
-	_parent = parent; //the collider that did the check
-	_other = other; //the collided-with collider
+	//_parent = parent; //the collider that did the check
+	//_other = other; //the collided-with collider
 
 	//calculate normal
 	float _x = other.position[0] - parent.position[0]; //gets a relative distance between the colliders as a position
 	float _y = other.position[1] - parent.position[1]; //same for y
 
 	//get angle of the two values
-	unsigned float _angle = std::atan(_y / _x);
+	float _angle = std::atan(_y / _x);
 
 	//set _x and _y to values where the magnitude is 1.
 	_x = std::cos(_angle);
@@ -24,26 +24,26 @@ Collision2D::Collision2D(Collider2D parent, Collider2D other) //constructor
 	_normal = { _x, _y };
 }
 
-std::vector<std::array<float, 2>> Collision2D::GetCollisionPoints() //NOT USED / CALCULATED YET
+std::vector<std::vector<float>> JCollision2D::GetCollisionPoints() //NOT USED / CALCULATED YET
 {
 	return _collisionPoints; //very simple
 }
 
-std::array<float, 2> Collision2D::GetNormal()
+std::vector<float> JCollision2D::GetNormal()
 {
 	return _normal; //very simple
 }
 
-CircleCollider2D::CircleCollider2D(float radius, float offsetX = 0, float offsetY = 0) //
+CircleJCollider2D::CircleJCollider2D(float radius, float offsetX, float offsetY) : _radius(radius), _offsetX(offsetX), _offsetY(offsetY)
 {
-	_radius = radius;
-	_offsetX = offsetX;
-	_offsetY = offsetY;
+	//_radius = radius;
+	//_offsetX = offsetX;
+	//_offsetY = offsetY;
 
 	position = { _offsetX, _offsetY };
 }
 
-void CircleCollider2D::SetRadius(float value) //allows for the changing of size for the circle collider
+void CircleJCollider2D::SetRadius(float value) //allows for the changing of size for the circle collider
 {
 	_radius = value; //set
 }
--- Jauntlet/Collision2D.h
+++ Jauntlet/Collision2D.h
@@ -4,32 +4,32 @@
 
 namespace Jauntlet
 {
-	class Collision2D //the returned class for collision data
+	class JCollider2D //Base class for all colliders
 	{
 	public:
-		Collision2D(Collider2D parent, Collider2D other); //constructor (requires the two colliding objects)
-		std::vector<std::array<float, 2>> GetCollisionPoints(); //not functional
-		std::array<float, 2> GetNormal(); //basically returns a "vector 2" regarding a direction (0,0 would be dead center, like they're on top of eachother) with a magnitude of 1.
-		//for the function above, if it returns 1,0 its facing straight to the right, 0,1 straight up, etc. etc., with values in between like at a 45 degree angle its somewhere around 0.7,0.7 but obviously the magnitude is exactly 1.
-	private:
-		std::vector<std::array<float, 2>> _collisionPoints; //still not functional; may also use a vector2 for positions in the future.
-		float _normal[2]; //stores the calculated normal at the point of collision
-		Collider2D _parent; //the collider doing the check
-		Collider2D _other; //the collider collided with during the check
-	};
-
-	class Collider2D //Base class for all colliders
-	{
-	public:
-		float position[2]; //could be a vector2 class, for now its an array of 2 floats. [0] = X [1] = Y.
+		std::vector<float> position; //could be a vector2 class, for now its an array of 2 floats. [0] = X [1] = Y.
 	protected:
 		//Sprite _linkedSprite;
 	};
 
-	class CircleCollider2D : Collider2D //Circle Collider, has a radius and otherwise is fairly simple. Collision determined by distance from center (hence the radius)
+	class JCollision2D //the returned class for collision data
 	{
 	public:
-		CircleCollider2D(float radius, float offsetX = 0, float offsetY = 0); //constructor; only radius is required but it will spawn at 0,0.
+		JCollision2D(JCollider2D parent, JCollider2D other); //constructor (requires the two colliding objects)
+		std::vector<std::vector<float>> GetCollisionPoints(); //not functional
+		std::vector<float> GetNormal(); //basically returns a "vector 2" regarding a direction (0,0 would be dead center, like they're on top of eachother) with a magnitude of 1.
+		//for the function above, if it returns 1,0 its facing straight to the right, 0,1 straight up, etc. etc., with values in between like at a 45 degree angle its somewhere around 0.7,0.7 but obviously the magnitude is exactly 1.
+	private:
+		std::vector<std::vector<float>> _collisionPoints; //still not functional; may also use a vector2 for positions in the future.
+		std::vector<float> _normal; //stores the calculated normal at the point of collision
+		JCollider2D _parent; //the collider doing the check
+		JCollider2D _other; //the collider collided with during the check
+	};
+
+	class CircleJCollider2D : JCollider2D //Circle Collider, has a radius and otherwise is fairly simple. Collision determined by distance from center (hence the radius)
+	{
+	public:
+		CircleJCollider2D(float radius, float offsetX = 0, float offsetY = 0); //constructor; only radius is required but it will spawn at 0,0.
 		void SetRadius(float value); //allows the changing of the radius
 
 	private:
