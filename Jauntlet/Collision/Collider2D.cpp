70ca5901d61543548bc9aa4be34cb77878810539
Chris p. Bacon <PS24CKowalewski@efcts.us>
2023-08-28 14:13:11 -0400

    add bocks collider

--- Jauntlet/Collision/Collider2D.cpp
+++ Jauntlet/Collision/Collider2D.cpp
@@ -12,4 +12,41 @@ CircleCollider2D::CircleCollider2D(float radius, float x, float y, float offsetX
 //allows for the changing of size for the circle collider
 void CircleCollider2D::SetRadius(float value) {
 	_radius = value;
+
+BoxCollider2D::BoxCollider2D(float width, float height, float x, float y, float offsetX = 0, float offsetY = 0) {
+	position = glm::vec2(x, y);
+	_size = glm::vec2(width, height);
+	_offset = glm::vec2(offsetX, offsetY);
+}
+
+BoxCollider2D::BoxCollider2D(glm::vec2 size, glm::vec2 pos, glm::vec2 offset) : _size(size), _offset(offset) {
+	position = pos;
+}
+
+float BoxCollider2D::GetWidth() {
+	return _size.x;
+}
+
+float BoxCollider2D::GetHeight() {
+	return _size.y;
+}
+
+glm::vec2 BoxCollider2D::GetSize() {
+	return _size;
+}
+
+void BoxCollider2D::SetWidth(float width) {
+	_size = glm::vec2(width, _size.y);
+}
+
+void BoxCollider2D::SetHeight(float height) {
+	_size = glm::vec2(_size.x, height);
+}
+
+void BoxCollider2D::SetSize(float width, float height) {
+	_size = glm::vec2(width, height);
+}
+
+void BoxCollider2D::SetSize(glm::vec2 size) {
+	_size = size;
 }
--- Jauntlet/Collision/Collider2D.h
+++ Jauntlet/Collision/Collider2D.h
@@ -23,4 +23,23 @@ public:
 private:
 	float _radius; //determines how far out to check for collision
 	glm::vec2 _offset; //Offset from parent object (if we parent it to something) for now determines position
+
+class BoxCollider2D : public Collider2D
+{
+public:
+	BoxCollider2D(float width, float height, float x, float y, float offsetX = 0, float offsetY = 0);
+	BoxCollider2D(glm::vec2 size, glm::vec2 pos, glm::vec2 offset);
+
+	float GetWidth();
+	float GetHeight();
+	glm::vec2 GetSize();
+
+	void SetWidth(float width);
+	void SetHeight(float height);
+	void SetSize(float width, float height);
+	void SetSize(glm::vec2 size);
+
+private:
+	glm::vec2 _size;
+	glm::vec2 _offset;
 };
