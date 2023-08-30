317b802e9322a8295df347114ea760af29f10ce4
Jack Kennedy <ps24jkennedy@efcts.us>
2023-08-30 13:29:17 -0400

    only run when we are in the build folder, cmake is successful and make is successful.

--- compile.sh
+++ compile.sh
@@ -2,8 +2,4 @@ sudo apt install make cmake libglm-dev libsdl2-dev libglew-dev g++
 #non debian nerds can die
 
 mkdir build
-cd build
-cmake ..
-make
-./game
-cd ..
+cd build && cmake .. && make && ./game && cd ..
