674d938cd0f5899eb5580012d00e05735efca7a2
Jack Kennedy <ps24jkennedy@efcts.us>
2023-08-30 08:27:10 -0400

    correct screen space conversion method
    Co-Authored-By: Xander Mooney <113313704+XanderMooney@users.noreply.github.com>

--- Jauntlet/Camera2D.cpp
+++ Jauntlet/Camera2D.cpp
@@ -28,7 +28,7 @@ void Camera2D::update() {
 
 glm::vec2 Camera2D::convertScreenToWorld(glm::vec2 screenCoords) {
 	// make 0 the center of the screen and invert the Y axis
-	screenCoords -= glm::vec2(_screenWidth / 2, (_screenHeight - screenCoords.y) / 2);
+	screenCoords += glm::vec2(_screenWidth / 2, _screenHeight / 2);
 	// scale the coords
 	screenCoords /= _scale;
 	// Translate based on camera position
