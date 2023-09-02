1cbe8a84aea1dd212f07dbf05a2bf2c0687736b9
Xander Mooney <ps24XMooney@efcts.us>
2023-09-01 20:18:07 -0400

    memory efficiency 🤓
    This may be undone at somepoint but this is technically more efficient than the easier to read method

--- Jauntlet/JMath.cpp
+++ Jauntlet/JMath.cpp
@@ -5,9 +5,6 @@ float JMath::Distance(glm::vec2 a, glm::vec2 b) {
 }
 
 float JMath::Distance(glm::vec3 a, glm::vec3 b) {
-	float dist1 = std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
-	if (a.z == b.z) { // skip the calculation if z's are the same
-		return dist1;
-	}
-	return std::sqrt(dist1 + std::pow(a.z + b.z, 2));
+	                           // a^2 + b^2 = c^2                                 // c^2 + z^2 = s^2
+	return std::sqrt(std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2)) + std::pow(a.z + b.z, 2));
 }
