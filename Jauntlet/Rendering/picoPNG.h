5d5489d4c3bd3d17bff0681ec5bcff02e0e64046
Jack Kennedy <ps24jkennedy@efcts.us>
2023-09-05 13:13:15 -0400

    std here

--- Jauntlet/Rendering/picoPNG.h
+++ Jauntlet/Rendering/picoPNG.h
@@ -6,5 +6,5 @@
 // THIS CODE IS NOT OURS
 
 namespace Jauntlet {
-extern int decodePNG(std::vector<unsigned char>& out_image, unsigned long& image_width, unsigned long& image_height, const unsigned char* in_png, size_t in_size, bool convert_to_rgba32 = true);
+extern int decodePNG(std::vector<unsigned char>& out_image, unsigned long& image_width, unsigned long& image_height, const unsigned char* in_png, std::size_t in_size, bool convert_to_rgba32 = true);
 }
