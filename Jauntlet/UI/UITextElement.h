74519e4520a4f8adb062b9d564699f5062c1f66b
Jack Kennedy <ps24jkennedy@efcts.us>
2023-09-09 14:27:10 -0400

    use color on uitextelement

--- Jauntlet/UI/UITextElement.h
+++ Jauntlet/UI/UITextElement.h
@@ -12,12 +12,12 @@ namespace Jauntlet {
 	{
 	public:
 		//constructor with _position;
-		UITextElement(SpriteFont* spriteFont,  std::string* text, float tint[]);
+		UITextElement(SpriteFont* spriteFont,  std::string* text, Color* color);
         void update(Camera2D* camera, SpriteBatch& spriteBatch, float scale); // wassup youtube today in minecraft we'll be getting spread wide open by c++ -jk
         // THE FUCKING &&&??????? AFTER THE TYPE????
     private:
 		std::string* _text;
         SpriteFont* _spriteFont;
-        GLfloat* _color;
+        Color* _color;
 	};
 }
