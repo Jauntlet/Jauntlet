79145be4f2a06e9243a0e8257a094f64bb98a2b2
Jack Kennedy <ps24jkennedy@efcts.us>
2023-09-11 13:56:42 -0400

    compile as debug for symbols

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 mkdir build
-cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && make && ./game && cd ..
+cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug && make && ./game && cd ..
