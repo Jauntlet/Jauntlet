c7c3a1e74b06745d65a448d8550d1d6ac01e6a15
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-09-14 14:07:24 -0400

    cleanup player.draw()

--- src/Player.cpp
+++ src/Player.cpp
@@ -46,10 +46,7 @@ void Player::update() {
 void Player::draw(Jauntlet::SpriteBatch& spriteBatch) {
 	glm::vec4 destRect = glm::vec4(_position.x, _position.y, 32, 32);
 
-	static Jauntlet::Color color;
-	color.setColor(255, 255, 255, 255);
-
-	spriteBatch.draw(destRect, glm::vec4(0,0,1,1), Jauntlet::ResourceManager::getTexture("Textures/Craig.png").id, 0, color);
+	spriteBatch.draw(destRect, glm::vec4(0,0,1,1), Jauntlet::ResourceManager::getTexture("Textures/Craig.png").id, 0, Jauntlet::Color(255, 255, 255, 255));
 }
 
 void Player::navigateTo(Jauntlet::TileMap& map, glm::vec2 position) {
