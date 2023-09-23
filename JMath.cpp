950ffbf1c15dc80dfbb5ced69ae6a86f78b2886a
Xander Mooney <ps24XMooney@efcts.us>
2023-09-22 21:51:38 -0400

    JMath Split

--- JMath.cpp
+++ JMath.cpp
@@ -1,5 +1,9 @@
 #include "JMath.h"
 
+#include <iostream> // REMOVE 
+
+std::string JMath::arr[2];
+
 float JMath::Distance(glm::vec2 a, glm::vec2 b) {
 	return std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
 }
@@ -7,4 +11,11 @@ float JMath::Distance(glm::vec2 a, glm::vec2 b) {
 float JMath::Distance(glm::vec3 a, glm::vec3 b) {
 	                           // a^2 + b^2 = c^2                                 // c^2 + z^2 = s^2
 	return std::sqrt(std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2)) + std::pow(a.z + b.z, 2));
+}
+
+std::string* JMath::Split(std::string& string, char delimiter) {
+	int delim = string.find(delimiter);
+	arr[0] = string.substr(0, delim);
+	arr[1] = delim == std::string::npos ? "" : string.substr(delim + 1, string.size() - delim);
+	return JMath::arr;
 }
--- JMath.h
+++ JMath.h
@@ -3,6 +3,7 @@
 // we want: JMath::Function(), not Jauntlet::JMath::Function().
 #include <cstddef>
 #include <glm/glm.hpp>
+#include <string>
 
 class JMath
 {
@@ -11,5 +12,9 @@ public:
 	static float Distance(glm::vec2 a, glm::vec2 b);
 	// find the distance between two vectors
 	static float Distance(glm::vec3 a, glm::vec3 b);
+	// splits a string in two, returns as an array of two elements [0] being before the delimiter, [1] being after.
+	static std::string* Split(std::string& string, char delimiter);
+private:
+	// used for splitting strings
+	static std::string arr[2];
 };
-
