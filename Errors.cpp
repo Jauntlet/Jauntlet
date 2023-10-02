60eb2fb23718c71cbc6f318973b3543bdf32352c
Xander Mooney <ps24XMooney@efcts.us>
2023-10-02 17:18:02 -0400

    Errors Cleanup/Sorting

--- Errors.cpp
+++ Errors.cpp
@@ -6,12 +6,14 @@
 
 // compiling on windows
 #ifdef _WIN32
-
 #include <Windows.h>
+#elif defined(__unix__)
+#include <unistd.h>
+#endif
 
 namespace Jauntlet {
 	void fatalError(std::string errorString) {
-		
+	#ifdef _WIN32
 		// test for if we are running with a terminal
 		if (GetFileType(GetStdHandle(STD_INPUT_HANDLE)) == FILE_TYPE_CHAR && GetFileType(GetStdHandle(STD_OUTPUT_HANDLE)) == FILE_TYPE_CHAR) {
 			std::cout << errorString << std::endl;
@@ -19,20 +21,7 @@ namespace Jauntlet {
 			int tmp;
 			std::cin >> tmp;
 		}
-		
-		SDL_Quit();
-		exit(-1);
-	}
-}
-
-// compiling on unix
-#elif defined(__unix__)
-
-#include <unistd.h>
-
-namespace Jauntlet {
-	void fatalError(std::string errorString) {
-
+	#elif defined(__unix__)
 		// test for if we are running with a terminal
 		if (isatty(fileno(stdin)) && isatty(fileno(stdout))) {
 			std::cout << errorString << std::endl;
@@ -40,22 +29,10 @@ namespace Jauntlet {
 			int tmp;
 			std::cin >> tmp;
 		}
+	#endif // if neither run, then the platform is unsupported. Support for these platforms may come if we get access to PCs with them.
 		
+		// always quit and exit SDL and the program
 		SDL_Quit();
 		exit(-1);
 	}
 }
-
-#else
-
-// unsupported platform; give no error messages and just let it quit.
-
-namespace Jauntlet {
-	void fatalError(std::string errorString) {
-		// despite us giving an errorString, we cannot show this error message unless we can ensure we are using a terminal.
-		SDL_Quit();
-		exit(-1);
-	}
-}
-
-#endif
