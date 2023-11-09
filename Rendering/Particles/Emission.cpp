d925ec550c5c591c04286d3427109979a7b4bf61
Jack Kennedy <ps24jkennedy@efcts.us>
2023-11-09 11:38:47 -0500

    center emissions

--- Rendering/Particles/Emission.cpp
+++ Rendering/Particles/Emission.cpp
@@ -9,7 +9,7 @@ Emission::Emission() {
 }
 
 void Emission::draw(SpriteBatch* spriteBatch, Camera2D* camera, GLuint texture) {
-    spriteBatch->draw(glm::vec4(position.x,position.y,size.x,size.y), texture, 0, Jauntlet::Color(255,255,255,255));
+    spriteBatch->draw(glm::vec4(position.x - size.x / 2.0f,position.y  - size.y / 2.0f,size.x,size.y), texture, 0, Jauntlet::Color(255,255,255,255));
 }
 
 void Emission::update() {
