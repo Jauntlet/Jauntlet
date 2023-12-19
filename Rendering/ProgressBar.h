2707f2b37bdf54d28c8c0117b10f5b0b2fbb7ec7
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-12-19 13:04:16 -0500

    UIProgressBarElement

--- Rendering/ProgressBar.h
+++ Rendering/ProgressBar.h
@@ -8,11 +8,11 @@ public:
 	ProgressBar(const std::string& texture1, const glm::vec4& texture1UV, const std::string& texture2, const glm::vec4& texture2UV, const glm::vec4& dest);
 	ProgressBar(const std::string& texture, const glm::vec4& texture1UV, const glm::vec4& texture2UV, const glm::vec4& dest);
 
+	virtual void draw(SpriteBatch& batch);
 
-	void draw(SpriteBatch& batch);
-private:
 	glm::vec2 position;
 	float progress = 0;
+protected:
 	GLuint _emptyTexture, _fullTexture;
 	glm::vec4 _UV1, _UV2;
 	glm::vec2 _size;
