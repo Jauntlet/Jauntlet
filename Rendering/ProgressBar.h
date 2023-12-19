f571d2f610c21c9fd912dfd16f2e1edb32bc9876
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-12-19 13:04:03 -0500

    Progress bar public position and progress

--- Rendering/ProgressBar.h
+++ Rendering/ProgressBar.h
@@ -8,19 +8,14 @@ public:
 	ProgressBar(const std::string& texture1, const glm::vec4& texture1UV, const std::string& texture2, const glm::vec4& texture2UV, const glm::vec4& dest);
 	ProgressBar(const std::string& texture, const glm::vec4& texture1UV, const glm::vec4& texture2UV, const glm::vec4& dest);
 
-	void setProgress(const float newProgress);
-	void setPosition(const glm::vec2& newPosition);
-
-	float getProgress() const;
-	glm::vec2 getPosition() const;
 
 	void draw(SpriteBatch& batch);
 private:
+	glm::vec2 position;
+	float progress = 0;
 	GLuint _emptyTexture, _fullTexture;
 	glm::vec4 _UV1, _UV2;
-	glm::vec2 _position, _size;
-
-	float _progress = 0;
+	glm::vec2 _size;
 };
 }
 
