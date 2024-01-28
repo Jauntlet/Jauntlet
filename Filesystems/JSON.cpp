b29f232df6214ba4daaf51aa35466a0a34268b0c
Xander Mooney <ps24XMooney@efcts.us>
2024-01-27 22:54:17 -0500

    YYJson wrapper for better syntax

---
+++ Filesystems/JSON.cpp
@@ -0,0 +1,60 @@
+#include "JSON.h"
+
+using namespace JSON;
+
+Document* JSON::readFile(const std::string& path, ReadFlag flag) {
+	return yyjson_read_file(path.data(), flag, NULL, NULL);
+}
+Document* JSON::readFile(const std::string& path) {
+	return yyjson_read_file(path.data(), 0, NULL, NULL);
+}
+Document* JSON::readFile(const char* path, ReadFlag flag) {
+	return yyjson_read_file(path, flag, NULL, NULL);
+}
+Document* JSON::readFile(const char* path) {
+	return yyjson_read_file(path, 0, NULL, NULL);
+}
+
+bool Value::toBool() {
+	return yyjson_get_bool(rawValue);
+}
+int Value::toInt() {
+	return yyjson_get_int(rawValue);
+}
+uint64_t Value::toUint() {
+	return yyjson_get_uint(rawValue);
+}
+size_t Value::getLength() {
+	return yyjson_get_len(rawValue);
+}
+double Value::toDouble() {
+	return yyjson_get_num(rawValue);
+}
+const char* Value::toRaw() {
+	return yyjson_get_raw(rawValue);
+}
+int64_t Value::toSint() {
+	return yyjson_get_sint(rawValue);
+}
+const char* Value::toString() {
+	return yyjson_get_str(rawValue);
+}
+
+const char* Value::getTypeDesc() {
+	return yyjson_get_type_desc(rawValue);
+}
+
+Value Value::operator[](int index) {
+	return yyjson_arr_get(rawValue, index);
+}
+Value Value::operator[](const char* value) {
+	return yyjson_obj_get(rawValue, value);
+}
+
+Value JSON::getRoot(Document* document) {
+	return Value(yyjson_doc_get_root(document));
+}
+
+Value JSON::OBJIterator::operator[](const char* value) {
+	return Value(yyjson_obj_iter_get(this, value));
+}
---
+++ Filesystems/JSON.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "../Externals/yyjson.h"
+#include <string>
+
+// Wrapper around yyjson to create a more readable format for use in code.
+// Meant to preserve the speed of yyjson, however may cause slight performance decreases.
+// to use yyjson without the wrapper, use `#include<Jauntlet/Externals/yyjson.h>, and follow its documentation here: https://ibireme.github.io/yyjson/doc/doxygen/html/md_doc__a_p_i.html
+namespace JSON {
+	typedef yyjson_doc Document;
+	typedef yyjson_read_flag ReadFlag;
+	//typedef yyjson_val Value;
+	
+	class Value {
+	public:
+		yyjson_val* rawValue;
+
+		Value(yyjson_val* rValue) : rawValue(rValue) {};
+
+		bool toBool();
+		int toInt();
+		uint64_t toUint();
+		size_t getLength();
+		double toDouble();
+		const char* toRaw();
+		int64_t toSint();
+		const char* toString();
+		
+		const char* getTypeDesc();
+
+		Value operator[](int index);
+		Value operator[](const char* value);
+	};
+
+	class OBJIterator : public yyjson_obj_iter {
+	public:
+		OBJIterator(Value object) : yyjson_obj_iter(yyjson_obj_iter_with(object.rawValue)) {};
+
+		Value operator[](const char* value);
+	};
+
+	Document* readFile(const std::string& path, ReadFlag flag);
+	Document* readFile(const std::string& path);
+	Document* readFile(const char* path, ReadFlag flag);
+	Document* readFile(const char* path);
+
+	Value getRoot(Document* document);
+}
