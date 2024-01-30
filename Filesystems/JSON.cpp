b551b0f66e4243340419a7a15482ceba316c76b1
Xander Mooney <thexandermooney@gmail.com>
2024-01-29 22:46:25 -0500

    More JSON work

---
+++ Filesystems/JSON.cpp
@@ -0,0 +1,95 @@
+#include "JSON.h"
+
+using namespace JSON;
+
+bool Value::toBool() {
+	return yyjson_get_bool(rawValue);
+}
+int Value::toInt() {
+	return yyjson_get_int(rawValue);
+}
+uint64_t Value::toUint() {
+	return yyjson_get_uint(rawValue);
+}
+size_t Value::getLength() {
+	return yyjson_get_len(rawValue);
+}
+double Value::toDouble() {
+	return yyjson_get_num(rawValue);
+}
+const char* Value::toRaw() {
+	return yyjson_get_raw(rawValue);
+}
+int64_t Value::toSint() {
+	return yyjson_get_sint(rawValue);
+}
+const char* Value::toString() {
+	return yyjson_get_str(rawValue);
+}
+
+const char* Value::getTypeDesc() {
+	return yyjson_get_type_desc(rawValue);
+}
+
+Value Value::operator[](int index) {
+	return yyjson_arr_get(rawValue, index);
+}
+Value Value::operator[](const char* value) {
+	return yyjson_obj_get(rawValue, value);
+}
+bool Value::operator!=(std::nullptr_t ptr) {
+	return ptr != rawValue;
+}
+bool Value::operator==(std::nullptr_t ptr) {
+	return ptr == rawValue;
+}
+
+Value Value::first() {
+	return yyjson_arr_get_first(rawValue);
+}
+Value Value::last() {
+	return yyjson_arr_get_last(rawValue);
+}
+
+Document::Document(const char* path, ReadFlag flag) 
+	:
+	rawValue(yyjson_read_file(path, flag, NULL, NULL))
+{
+	// Empty
+}
+Document::Document(std::string path, ReadFlag flag)
+	:
+	rawValue(yyjson_read_file(path.data(), flag, NULL, NULL)) 
+{
+	// Empty
+}
+
+Value Document::getRoot() { 
+	return yyjson_doc_get_root(rawValue); 
+}
+void Document::writeTo(const char* path, WriteFlag flag = 0) { 
+	yyjson_write_file(path, rawValue, flag, NULL, NULL); 
+}
+
+MutableValue MutableDocument::getRoot() { 
+	return MutableValue(yyjson_mut_doc_get_root(rawValue)); 
+}
+void MutableDocument::clearRoot() { 
+	yyjson_mut_doc_set_root(rawValue, NULL); 
+}
+
+void MutableDocument::writeTo(const char* path, WriteFlag flag = 0) { 
+	yyjson_mut_write_file(path, rawValue, flag, NULL, NULL); 
+}
+
+void MutableValue::setDocRoot(MutableDocument& doc) { 
+	yyjson_mut_doc_set_root(doc.rawValue, rawValue); 
+}
+
+Value OBJIterator::operator[](const char* value) {
+	return Value(yyjson_obj_iter_get(this, value));
+}
+
+Value OBJIterator::next() {
+	return yyjson_obj_iter_next(this);
+}
--- Filesystems/JSON.h
+++ Filesystems/JSON.h
@@ -19,112 +19,86 @@ namespace JSON {
 
 		// returns the contents if the value is a Bool
 		// returns NULL if value is not a bool, or is NULL
-		bool toBool() {
-			return yyjson_get_bool(rawValue);
-		}
+		bool toBool();
 		// returns the contents if the value is an Int
 		// returns 0 if value is NULL or not an int
-		int toInt() {
-			return yyjson_get_int(rawValue);
-		}
+		int toInt();
 		// returns the contents if the value is an uint64_t
 		// returns 0 if value is NULL or not an uint64_t
-		uint64_t toUint() {
-			return yyjson_get_uint(rawValue);
-		}
+		uint64_t toUint();
 		// returns the contents length (string, array, object)
 		// returns 0 if value is NULL or not a valid option
-		size_t getLength() {
-			return yyjson_get_len(rawValue);
-		}
+		size_t getLength();
 		// returns the contents if the value is a double
 		// returns 0.0 if value is NULL or not a double
-		double toDouble() {
-			return yyjson_get_num(rawValue);
-		}
+		double toDouble();
 		// returns the contents if the value is raw
 		// returns NULL if value is NULL or not raw
-		const char* toRaw() {
-			return yyjson_get_raw(rawValue);
-		}
+		const char* toRaw();
 		// returns the contents if the value is an int64_t
 		// returns 0 if value is NULL or not an int64_t
-		int64_t toSint() {
-			return yyjson_get_sint(rawValue);
-		}
+		int64_t toSint();
 		// returns the contents if the value is a string
 		// returns NULL if value is NULL or not a string
-		const char* toString() {
-			return yyjson_get_str(rawValue);
-		}
+		const char* toString();
 
 		// Returns the values JSON type as a readable string.
 		// meant for debugging
-		const char* getTypeDesc() {
-			return yyjson_get_type_desc(rawValue);
-		}
+		const char* getTypeDesc();
 
 		// returns the first element in an array.
 		// returns NULL if element is not an array.
-		Value first() {
-			return yyjson_arr_get_first(rawValue);
-		}
+		Value first();
 		// returns the last element in an array.
 		// returns NULL if element is not an array.
-		Value last() {
-			return yyjson_arr_get_last(rawValue);
-		}
+		Value last();
 
-		Value operator[](int index) {
-			return yyjson_arr_get(rawValue, index);
-		}
-		Value operator[](const char* value) {
-			return yyjson_obj_get(rawValue, value);
-		}
-		bool operator!=(std::nullptr_t ptr) {
-			return ptr != rawValue;
-		}
-		bool operator==(std::nullptr_t ptr) {
-			return ptr == rawValue;
-		}
+		Value operator[](int index);
+		Value operator[](const char* value);
+		bool operator!=(std::nullptr_t ptr);
+		bool operator==(std::nullptr_t ptr);
 	};
 
 	// The main document that holds all of the JSON values and strings.
 	class Document {
 	public:
+		// The raw value of the Document; used for calls directly to yyjson
 		yyjson_doc* rawValue;
 
-		Document(yyjson_doc* rValue) : rawValue(rValue) {}
-		Document(const char* path, ReadFlag flag = 0) : rawValue(yyjson_read_file(path, flag, NULL, NULL)) {};
-		Document(std::string path, ReadFlag flag = 0) : rawValue(yyjson_read_file(path.data(), flag, NULL, NULL)) {};
-		~Document() { yyjson_doc_free(rawValue); }
+		Document(yyjson_doc* rValue) : rawValue(rValue) {};
+		Document(const char* path, ReadFlag flag = 0);
+		Document(std::string path, ReadFlag flag = 0);
+		~Document() { yyjson_doc_free(rawValue); };
 
-		void writeTo(const char* path, WriteFlag flag = 0) { 
-			yyjson_write_file(path, rawValue, flag, NULL, NULL);
-		}
-		Value getRoot() {
-			return Value(yyjson_doc_get_root(rawValue));
-		}
+		// Returns the root of the Document
+		Value getRoot();
+
+		// Saves the document to the specified path.
+		// Use MutableDocument for an editable JSON document.
+		void writeTo(const char* path, WriteFlag flag = 0);
 
 		// prevents accidental copying of documents
 		Document(const Document& other) = delete;
 		Document& operator=(const Document& other) = delete;
 	};
 	
+	class MutableValue;
+
 	class MutableDocument {
 	public:
+		// The raw value of the Document; used for calls directly to yyjson
 		yyjson_mut_doc* rawValue;
 
-		MutableDocument() {
-			rawValue = yyjson_mut_doc_new(NULL);
-		}
-		~MutableDocument() { 
-			yyjson_mut_doc_free(rawValue);
-		}
+		MutableDocument() { rawValue = yyjson_mut_doc_new(NULL); }
+		~MutableDocument() { yyjson_mut_doc_free(rawValue); }
 
-		void writeTo(const char* path, WriteFlag flag = 0) { 
-			yyjson_mut_write_file(path, rawValue, flag, NULL, NULL);
-		}
+		// Returns the root of the Document
+		MutableValue getRoot();
+		// Clears the root of the Document
+		void clearRoot();
+
+		// Saves the document to the specified path.
+		void writeTo(const char* path, WriteFlag flag = 0);
 
 		// prevents accidental copying of MutableDocuments
 		MutableDocument(const MutableDocument& other) = delete;
@@ -135,55 +109,39 @@ namespace JSON {
 	public:
 		yyjson_mut_val* rawValue;
 
-		MutableValue(MutableDocument& doc, const char* string) { 
-			rawValue = yyjson_mut_str(doc.rawValue, string);
-		}
-		MutableValue(MutableDocument& doc, int integer) { 
-			rawValue = yyjson_mut_int(doc.rawValue, integer);
-		}
-		void setDocRoot(MutableDocument& doc) { 
-			yyjson_mut_doc_set_root(doc.rawValue, rawValue);
-		}
+		MutableValue(MutableDocument& doc, const char* string) { rawValue = yyjson_mut_str(doc.rawValue, string); }
+		MutableValue(MutableDocument& doc, int integer) { rawValue = yyjson_mut_int(doc.rawValue, integer); }
+		MutableValue(yyjson_mut_val* rValue) : rawValue(rValue) {};
+		
+		// sets the value to be the root of the MutableDocument
+		void setDocRoot(MutableDocument& doc);
 	};
 
 	class MutableArray {
 	public:
 		yyjson_mut_val* rawValue;
 
-		MutableArray(MutableDocument& doc) { 
-			rawValue = yyjson_mut_arr(doc.rawValue);
-		}
-		void append(JSON::MutableValue& val) { 
-			yyjson_mut_arr_append(rawValue, val.rawValue);
-		}
-		void setDocRoot(MutableDocument& doc) { 
-			yyjson_mut_doc_set_root(doc.rawValue, rawValue);
-		}
+		MutableArray(MutableDocument& doc) { rawValue = yyjson_mut_arr(doc.rawValue); }
+		
+		void append(JSON::MutableValue& val) { yyjson_mut_arr_append(rawValue, val.rawValue); }
+		void setDocRoot(MutableDocument& doc) { yyjson_mut_doc_set_root(doc.rawValue, rawValue); }
 	};
 
 	class MutableObject {
 	public:
 		yyjson_mut_val* rawValue;
 		
-		MutableObject(MutableDocument& doc) { 
-			rawValue = yyjson_mut_obj(doc.rawValue); 
-		}
-		void setDocRoot(MutableDocument& doc) {
-			yyjson_mut_doc_set_root(doc.rawValue, rawValue); 
-		}
+		MutableObject(MutableDocument& doc) { rawValue = yyjson_mut_obj(doc.rawValue); }
+		void setDocRoot(MutableDocument& doc) { yyjson_mut_doc_set_root(doc.rawValue, rawValue); }
 	};
 
 	// Made for faster iteration through an object. Best to use when iterating through a single object many times over.
 	class OBJIterator : public yyjson_obj_iter {
 	public:
-		OBJIterator(Value object) : yyjson_obj_iter(yyjson_obj_iter_with(object.rawValue)) {}
+		OBJIterator(Value object) : yyjson_obj_iter(yyjson_obj_iter_with(object.rawValue)) {};
 		
-		Value next() {
-			return yyjson_obj_iter_next(this);
-		}
+		Value next();
 
-		Value operator[](const char* value) {
-			return Value(yyjson_obj_iter_get(this, value));
-		}
+		Value operator[](const char* value);
 	};
 }
